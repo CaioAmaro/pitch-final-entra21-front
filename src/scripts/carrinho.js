3cc1d424abdf6f5f79c28cb322dd590fe0204e72
Jackson Andre Lourenco <107936025+jacksonlourenco@users.noreply.github.com>
2025-08-20 08:57:02 -0300

    20/08

---
+++ src/scripts/carrinho.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("cartButton").addEventListener("click", function () {
+        $("#cartModal").modal("show");
+    });
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,7 @@
 import './homepage.js';
 import './login.js';
+import './filtroProduto.js';
 import './loadHeader.js';
 import './loadFooter.js';
 import './resultado.js';
+import './carrinho.js';
